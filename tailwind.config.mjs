08aea6432fc7907112863855d23ca7df39f8d7a3
Mohit <mohit@hey.com>
2024-01-30 11:55:28 +0530

    x

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-
-			colors: {
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
 
 				"mc_slate": "#5b626d",
 				"mc_black": "#222b37",
@@ -19,8 +46,7 @@ export default {
 				"merriweather": [ "Merriweather", "serif" ]
 
 			}
-
-		},
-	},
-	plugins: [],
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 }
