6bd4c50576fd8ef1cb0ef46a9b2141bd8aca8219
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-02 12:12:53 +0530

    feat: consumed home API

---
+++ src/api/home/index.js
@@ -0,0 +1,40 @@
+import { endpointFetch } from "../../functions/endpoint"
+
+export const homeAPI = async ( environment ) => {
+
+    const endpoint = endpointFetch( environment )
+
+    // NETWORK CALL TO GET DATA
+    const network_data = await fetch( endpoint + "/home" )
+    const data_json = await network_data.json()
+
+    const { home, services, meta } = data_json.data
+
+    let home_data = {
+
+        // HOME PROPS
+        hero : "",
+        clients : "",
+        about : "",
+        work_with_us : "",
+        services : ""
+
+    }
+    home.map( ( data ) => {
+
+        if( data.slug === "hero" )
+            home_data.hero = data
+        else if( data.slug === "clients" )
+            home_data.clients = data
+        else if ( data.slug === "about" )
+            home_data.about = data
+        else if ( data.slug === "work-with-us" )
+            home_data.work_with_us = data
+        else if ( data.slug === "services" )
+            home_data.services = data
+
+    })
+
+    return { home_data, services, meta }
+
+}
