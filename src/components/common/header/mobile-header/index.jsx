dc9f5c996a1b9a8e085a2cf508047bddd31b0965
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-12 02:46:12 +0530

    fix: fixed header menu

--- src/components/common/header/mobile-header/index.jsx
+++ src/components/common/header/mobile-header/index.jsx
@@ -5,15 +5,15 @@ import { motion, AnimatePresence } from "framer-motion"
 import { useState } from "react"
 
 // IMPORTS COMPONENTS
-import HeaderMobileItem from "./li/index"
+import Li from "./li/index"
 
 const HeaderMobile = ( props ) => {
 
     const [ isOpen, updateOpen ] = useState( false )
 
     return (
-        <header className="md:hidden bg-white px-6 h-14 py-3 z-20 w-full">
-            <div className="flex justify-between items-center z-20">
+        <header className="md:hidden bg-white px-4 z-[100] py-4 border-b border-gray-100">
+            <div className="flex justify-between items-center relative">
                 <a href="/">
                     <div className="w-24 aspect-[5/2]">
                         <img
@@ -23,31 +23,25 @@ const HeaderMobile = ( props ) => {
                         />
                     </div>
                 </a>
-                <div className="flex items-center gap-4">
-                    <div>
-                        <p className="text-xs text-mc_slate font-graphik">Sales Hotline (worldwide)</p>
-                        <p className="text-sm text-mc_slate font-graphik">+4908926200255</p>
-                    </div>
-                    <div onClick={ () => updateOpen( !isOpen ) }>
-                        {
+                <div onClick={ () => updateOpen( !isOpen ) }>
+                    {
 
-                            isOpen &&
-                            <img
-                                src="/icons/close.svg"
-                                className="w-6 aspect-square"
-                            />
+                        isOpen &&
+                        <img
+                            src="/icons/close.svg"
+                            className="w-6 aspect-square"
+                        />
 
-                        }
-                        {
+                    }
+                    {
 
-                            !isOpen &&
-                            <img
-                                src="/icons/menu.svg"
-                                className="w-5 aspect-square"
-                            />
+                        !isOpen &&
+                        <img
+                            src="/icons/menu.svg"
+                            className="w-5 aspect-square"
+                        />
 
-                        }
-                    </div>
+                    }
                 </div>
             </div>
             <AnimatePresence initial={false}>
@@ -65,13 +59,13 @@ const HeaderMobile = ( props ) => {
                         transition={{ duration: 0.3 }}
                         className="grow"
                     >
-                        <div className="flex flex-col gap-1 items-center justify-center h-full z-10">
+                        <div className={`flex flex-col gap-1 items-center justify-center h-full relative ${ isOpen ? "z-[100]" : "hidden"}`}>
                             <ul className="flex flex-col justify-center items-center gap-8">
-                                <HeaderMobileItem href="/">Services</HeaderMobileItem>
-                                <HeaderMobileItem href="/">Industries</HeaderMobileItem>
-                                <HeaderMobileItem href="/">Publications</HeaderMobileItem>
-                                <HeaderMobileItem href="/">Home</HeaderMobileItem>
-                                <HeaderMobileItem href="/">About Us</HeaderMobileItem>
+                                <Li>Services</Li>
+                                <Li>Industries</Li>
+                                <Li>Publications</Li>
+                                <Li href="/">Home</Li>
+                                <Li href="/about-us">About Us</Li>
                             </ul>
                         </div>
                     </motion.div>
